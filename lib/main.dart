9eb6d0226285bdf0d3e87050e00934911c46ecde
r-fine <saiadul.arfain@tuta.io>
2022-05-23 19:02:18 +0600

    migrated to auth service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/services/auth/auth_service.dart';
 
 import '../constants/routes.dart';
-import '../firebase_options.dart';
 import '../views/login_view.dart';
 import '../views/register_view.dart';
 import '../views/verify_email_view.dart';
@@ -34,15 +32,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
+      future: AuthService.firebase().initialize(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
+            final user = AuthService.firebase().currentUser;
             if (user != null) {
-              if (user.emailVerified) {
+              if (user.isEmailVerified) {
                 return const NotesView();
               } else {
                 return const VerifyEmailView();
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/services/auth/auth_service.dart';
 
 import '../constants/routes.dart';
 
@@ -27,14 +27,13 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
                 "If you haven't received a verification email yet, press the button below"),
             TextButton(
               onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
+                AuthService.firebase().sendEmailVerification();
               },
               child: const Text('Send email verification'),
             ),
             TextButton(
               onPressed: () async {
-                await FirebaseAuth.instance.signOut();
+                await AuthService.firebase().logOut();
                 Navigator.of(context).pushNamedAndRemoveUntil(
                   registerRoute,
                   (route) => false,
